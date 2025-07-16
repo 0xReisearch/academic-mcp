f1c058e82d052672ed68ec65c8959ffccb2eedfa
0xreisearch <0xreisearch@proton.me>
2025-07-16 17:57:25 +0200

    Fix arxiv categories

--- src/index.ts
+++ src/index.ts
@@ -18,6 +18,31 @@ const pdfUtils = PDFUtils.getInstance();
 
 const tools: Tool[] = [
   {
+    name: 'search_arxiv_by_category',
+    description: 'Search ArXiv papers by specific category using exact category codes.',
+    inputSchema: {
+      type: 'object',
+      properties: {
+        category: {
+          type: 'string',
+          description: 'ArXiv category code. Available categories: cs.AI, cs.LG, cs.CV, cs.CL, cs.CR, cs.DS, cs.RO, cs.NE, cs.GT, cs.HC, cs.IR, cs.IT, cs.MA, cs.MM, cs.NI, cs.OS, cs.PF, cs.PL, cs.SC, cs.SE, cs.SI, cs.SY, quant-ph, math.AG, math.AT, math.AP, math.CA, math.CO, math.CT, math.CV, math.DG, math.DS, math.FA, math.GM, math.GN, math.GR, math.GT, math.HO, math.IT, math.KT, math.LO, math.MG, math.MP, math.NA, math.NT, math.OA, math.OC, math.PR, math.QA, math.RA, math.RT, math.SG, math.SP, math.ST, physics.acc-ph, physics.ao-ph, physics.app-ph, physics.atm-clus, physics.atom-ph, physics.bio-ph, physics.chem-ph, physics.class-ph, physics.comp-ph, physics.data-an, physics.ed-ph, physics.flu-dyn, physics.gen-ph, physics.geo-ph, physics.hist-ph, physics.ins-det, physics.med-ph, physics.optics, physics.plasm-ph, physics.pop-ph, physics.soc-ph, physics.space-ph, astro-ph.CO, astro-ph.EP, astro-ph.GA, astro-ph.HE, astro-ph.IM, astro-ph.SR, cond-mat.dis-nn, cond-mat.mes-hall, cond-mat.mtrl-sci, cond-mat.other, cond-mat.quant-gas, cond-mat.soft, cond-mat.stat-mech, cond-mat.str-el, cond-mat.supr-con, q-bio.BM, q-bio.CB, q-bio.GN, q-bio.MN, q-bio.NC, q-bio.OT, q-bio.PE, q-bio.QM, q-bio.SC, q-bio.TO, stat.AP, stat.CO, stat.ME, stat.ML, stat.OT, stat.TH, econ.EM, econ.GN, econ.TH, eess.AS, eess.IV, eess.SP, eess.SY, hep-ex, hep-lat, hep-ph, hep-th, math-ph, nlin.AO, nlin.CD, nlin.CG, nlin.PS, nlin.SI, nucl-ex, nucl-th, q-fin.CP, q-fin.EC, q-fin.GN, q-fin.MF, q-fin.PM, q-fin.PR, q-fin.RM, q-fin.ST, q-fin.TR',
+        },
+        maxResults: {
+          type: 'number',
+          description: 'Maximum number of results to return (default: 10)',
+          default: 10,
+        },
+        sortBy: {
+          type: 'string',
+          enum: ['relevance', 'lastUpdatedDate', 'submittedDate'],
+          description: 'Sort results by relevance, last updated date, or submitted date',
+          default: 'submittedDate',
+        },
+      },
+      required: ['category'],
+    },
+  },
+  {
     name: 'search_arxiv',
     description: 'Search for scientific papers on ArXiv. The search is optimized to handle common topics like "quantum computing", "machine learning", "artificial intelligence", etc. and will automatically use appropriate ArXiv categories for better results. You can also use ArXiv syntax directly (cat:, ti:, abs:, au:).',
     inputSchema: {
